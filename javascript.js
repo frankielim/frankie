36035ac1cdafbfb5e1ea5a0f68839e0f699fc9c2
frankielim <33901195+frankielim@users.noreply.github.com>
2017-11-23 00:33:28 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+/* 
+    IBM In-house {Web Coder} course Assignment Snippet.
+    JavaScript file.
+    For internal IBM use.
+*/
+
+// JavaSCript goes here...
+
+<script>
+$(function () {
+ $('[data-toggle="popover"]').popover()
+})
+
+</script>
